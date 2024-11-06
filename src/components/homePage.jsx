740e74228463f8115f9a767d34facc24c3633e3a
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-11-06 23:27:07 +0530

    todo list

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,32 +1,49 @@
+import { useState } from "react";
 
+const HomePage = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-const homePage = ()=>{
-    return (
-        <div className="app">
-          <div className="mainHeading">
-            <h1>ToDo List</h1>
-          </div>
-          <div className="subHeading">
-            <br />
-            <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-          </div>
-          <div className="input">
-            <input type="text" placeholder="🖊️ Add item..." />
-            <i className="fas fa-plus"></i>
-          </div>
-          <div className="todos">
-            <div className="todo">
-              <div className="left">
-                <input type="checkbox" name="" id="" />
-                <p>Rect tutorial</p>
-              </div>
-              <div className="right">
-                <i className="fas fa-times"></i>
-              </div>
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo(''); // Clear input after adding
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕</h2>
+      </div>
+      <div className="input">
+        <input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {todos.map((todo, index) => (
+          <div key={index} className="todo">
+            <div className="left">
+              <input type="checkbox" />
+              <p>{todo}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
             </div>
           </div>
-        </div>
-      );
-}
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default homePage
+export default HomePage;
